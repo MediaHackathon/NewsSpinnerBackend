942bb77949832bc6eac51b2a5cb241b2e99df762
flymithra <vampcore1@gmail.com>
2017-06-10 04:31:01 +0600

    keke

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require('express')
 const app = express();
 const dbManager = require('./dbManager');
 const bodyParser = require('body-parser');
+const request = require('request');
+
 
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
@@ -13,7 +15,27 @@ app.get('/getRequests', (req, res) => {
     })
 })
 
-app.post('/addRequest', )
+app.post('/addRequests', (req, res) => {
+  const input = req.body.input;
+  request.get(`https://lenta.ru/search/v2/process?from=0&size=10&sort=2&title_only=0&domain=1&modified%2Cformat=yyyy-MM-dd&query=${input}`, (err, data) => {
+    const articles = JSON.parse(data.body).matches;
+    const testReg = new RegExp(input, 'ig');
+    const result = [];
+    articles.forEach((article) => {
+      if (testReg.test(article.title)) {
+        result.push(
+          {
+            text: article.text,
+            title: article.title,
+            url: article.url,
+            timestamp: article.modified
+          }
+        )
+      };
+    })
+    res.send(result);
+  })
+})
 
 app.listen(3000, () => {
   console.log('Example app listening on port 3000!')
